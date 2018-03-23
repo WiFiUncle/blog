fe2b28e1e87a879a3cac0426b41fd643cbbef17a
wuxian <xian.wu@visualtouring.com>
2018-03-23 13:58:31 +0800

    添加.ignore文件，新增分享书籍

--- babelDemo/main.js
+++ babelDemo/main.js
@@ -1,7 +1,7 @@
 /**
  * Created By WiFi ON 2018/1/12 13:39
  */
-const NAME = 'My name is W33iFi_Uncle';
+const NAME = 'My name is W330000000iFi_Uncle';
 let func = () => {
     console.log(NAME);
 };
